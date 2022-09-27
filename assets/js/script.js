0eddefa48a1bdd5da5e1dce6c999da822661254d
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-26 23:11:19 -0500

    fix script link error and duplicate timeslots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-let currentDayEl = $('#currentDay');
-
+const currentDayEl = $("#currentDay");
+console.log(currentDayEl);
 function currentDay() {
-    let today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
-    currentDayEl.text(today)
-};
+  let today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+  currentDayEl.text(today);
+}
 
 $(document).ready(currentDay());
